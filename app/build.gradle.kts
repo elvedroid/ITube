a88a536a1c43efe598e705a5ee48af3de74e8632
Elvedin Selimoski <elveselimoski@gmail.com>
2022-10-11 21:43:26 +0200

    Add debug build type and debug signing config for release build type

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,12 +19,19 @@ android {
     }
 
     buildTypes {
+        val debug by getting {
+            applicationIdSuffix = ".debug"
+        }
         val release by getting {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            // To publish on the Play store a private signing key is required, but to allow anyone
+            // who clones the code to sign and run the release variant, use the debug signing key.
+            // TODO: Abstract the signing configuration to a separate file to avoid hardcoding this.
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 
